064dd50d35761bff07680121c2669d3dbc91cc08
alu0101319001 <alu0101319001@ull.edu.es>
2024-05-31 16:17:14 +0100

    Update pip

--- startup.sh
+++ startup.sh
@@ -16,7 +16,7 @@ python3 -m venv venv
 source venv/bin/activate
 
 # Instalar Django y otras dependencias del proyecto Django
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 # Mostrar mensaje de finalización
 echo "Instalación completada. Ahora puedes empezar a trabajar en tu proyecto Django y Ansible."
