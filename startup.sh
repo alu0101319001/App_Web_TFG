52867bee923812340c0fe0b44676fea8da2ccb5c
alu0101319001 <alu0101319001@ull.edu.es>
2024-05-31 16:14:07 +0100

    Solo instalaciones

--- startup.sh
+++ startup.sh
@@ -2,18 +2,10 @@
 
 # Actualizar los paquetes del sistema
 sudo apt update
-
 # Instalar git
 sudo apt install -y git
-
-# Clonar el repositorio
-git config user.name "alu0101319001"
-git config user.email alu0101319001@ull.edu.es
-git clone https://github.com/alu0101319001/App_Web_TFG.git proyecto_tfg_admin
-
 # Entrar al directorio del proyecto
 cd proyecto_tfg_admin
-
 # Instalar Python y pip
 sudo apt install -y python3 python3-pip
 # Instalar Ansible
