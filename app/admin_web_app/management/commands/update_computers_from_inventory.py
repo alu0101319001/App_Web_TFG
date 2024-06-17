cc993174bc19e1a280e76551b781fc491f458b85
alu0101319001 <alu0101319001@ull.edu.es>
2024-06-17 17:35:40 +0100

    Solve Database

--- app/admin_web_app/management/commands/update_computers_from_inventory.py
+++ app/admin_web_app/management/commands/update_computers_from_inventory.py
@@ -1,3 +1,4 @@
+# admin_web_app/management/commands/update_computers.py
 import os
 import time
 from io import StringIO
@@ -36,15 +37,13 @@ class Command(BaseCommand):
         log[time.time()] = 'Updating info for online computers...'
         for host, details in online_computers.items():
             log[time.time()] = f'\tCalling update for: {host}'
-            output = self.update_computer(host, details, 'online')
-            log[time.time()] = output
-
+            self.update_computer(host, details, 'online')
+        
         # Iterar sobre los ordenadores fuera de línea y actualizar la base de datos
         log[time.time()] = 'Updating info for offline computers...'
         for host, details in offline_computers.items():
             log[time.time()] = f'\tCalling update for: {host}'
-            output = self.update_computer(host, details, 'offline')
-            log[time.time()] = output
+            self.update_computer(host, details, 'offline')
 
         log[time.time()] = 'Computers updated successfully.\nEND'
         sorted_log = dict(sorted(log.items()))
@@ -62,23 +61,37 @@ class Command(BaseCommand):
         # Obtén la dirección IP solo si el estado es "online"
         ip = self.get_value_from_string(details, 'ansible_host') if status.lower() == 'online' else None
 
-        # Busca si existe un registro con la misma MAC
+        # Consulta el objeto existente de Computer si está presente
         existing_computer = Computer.objects.filter(mac=mac).first()
+
+        # Determina el valor de warning según la lógica deseada
         if existing_computer:
-            # Si existe, mantén el valor de warning y asigna el icono correspondiente
+            # Si ya existe un registro para esta MAC, mantener el estado actual de warning
             warning = existing_computer.warning
-            icon = 'computer--exclamation.png' if warning else ('computer.png' if status.lower() == 'online' else 'computer-off.png')
         else:
-            # Si no existe, asigna el icono predeterminado
-            warning = False
+            # Si no existe, establece el valor predeterminado de warning
+            warning = False  # Ajusta esto según tu lógica inicial
+
+        # Determina el icono basado en el estado de warning
+        if warning:
+            icon = 'computer--exclamation.png' # if status.lower() == 'online' else 'computer-warning-off.png'
+        else:
             icon = 'computer.png' if status.lower() == 'online' else 'computer-off.png'
 
-        # Insertar o actualizar el registro en la base de datos
+        # Crear o obtener el registro de Computer con los datos actualizados
+        computer_defaults = {
+            'state': state,
+            'ip': ip,
+            'icon': icon,
+            'warning': warning,  # Mantener el estado actual si no se proporciona explícitamente
+        }
+
+        # Actualiza o crea el objeto Computer
         output = f'\tUpdating data: {name}--{state}--{icon}--{mac}--{ip}'
-        computer = Computer(name=name, state=state, icon=icon, mac=mac, ip=ip, warning=warning)
-        computer.save()
+        Computer.get_or_create_by_name_and_mac(name, mac, computer_defaults)
         output += f'\n\tInventory computers created successfully for {name}'
         return output
+
 
     def get_value_from_string(self, string, key):
         # Parsea el valor de la cadena para obtener el valor de la clave especificada
--- app/admin_web_app/models.py
+++ app/admin_web_app/models.py
@@ -4,10 +4,31 @@ from django.db import models
 class Computer(models.Model):
     name = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
-    icon =  models.ImageField(upload_to='img/', blank=True, null=True)  # Imagen del estado
-    mac = models.CharField(max_length=17, blank=True, null=True)  # Dirección MAC
+    icon = models.ImageField(upload_to='img/', blank=True, null=True)  # Imagen del estado
+    mac = models.CharField(max_length=17, blank=True, null=True, unique=True)  # Dirección MAC como único
     ip = models.GenericIPAddressField(protocol='both', unpack_ipv4=False, blank=True, null=True)  # Dirección IP
     warning = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
+
+    @classmethod
+    def get_or_create_by_name_and_mac(cls, name, mac, defaults=None):
+        """
+        Obtener o crear un objeto Computer basado en el nombre y la MAC.
+        Si ya existe un objeto con el mismo nombre y MAC, devuelve el objeto existente.
+        Si no existe, crea un nuevo objeto con los datos proporcionados.
+        """
+        defaults = defaults or {}
+        obj, created = cls.objects.get_or_create(name=name, mac=mac, defaults=defaults)
+        
+        if not created:
+            # Si el objeto ya existía, mantener el valor actual de 'warning' si no está presente en defaults
+            if 'warning' not in defaults:
+                defaults['warning'] = obj.warning  # Mantener el valor actual de 'warning'
+
+            for key, value in defaults.items():
+                setattr(obj, key, value)
+            obj.save()
+
+        return obj
